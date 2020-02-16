e5ec50cdb2e812bba3316a5b93de60f4eb9939bf
Dominik <d.beczyczko@gmail.com>
2020-02-16 19:28:47 +0100

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        const c = document.getElementById('board-canvas');
+        const ctx = c.getContext('2d');
+        ctx.font = '30px Arial';
+        ctx.fillText('Hello World', 50, 50);
+    }, []);
+
+    return (
+        <div className="App">
+            <canvas id="board-canvas" width="800" height="600">
+                Your browser does not support the HTML5 canvas tag.
+            </canvas>
+        </div>
+    );
 }
 
 export default App;
