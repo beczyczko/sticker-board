d7d73634a9bd61a8a1830f2b1002ce0f2da42cec
Dominik <d.beczyczko@gmail.com>
2020-02-23 09:49:03 +0100

    add sticker dragging

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import stickerImage from './assets/sticker.png';
 function App() {
 
     let TextureCache = PIXI.utils.TextureCache;
+    let stage;
 
     const loader = new PIXI.Loader();
     loader
@@ -18,17 +19,63 @@ function App() {
         const textureCacheElement = TextureCache['sticker'];
         console.log(textureCacheElement);
 
-        const sticker = new PIXI.TilingSprite(textureCacheElement);
+        const sticker = new PIXI.Sprite(textureCacheElement);
         sticker.width = sticker.texture.width;
         sticker.height = sticker.texture.height;
 
-        sticker.scale = { x: 0.25, y: 0.25 };
+        sticker.scale.set(0.25);
+        sticker.interactive = true;
+        sticker.buttonMode = true;
+        sticker.anchor.set(0.5);
+
+        sticker
+            // events for drag start
+            .on('mousedown', onDragStart)
+            .on('touchstart', onDragStart)
+            // events for drag end
+            .on('mouseup', onDragEnd)
+            .on('mouseupoutside', onDragEnd)
+            .on('touchend', onDragEnd)
+            .on('touchendoutside', onDragEnd)
+            // events for drag move
+            .on('mousemove', onDragMove)
+            .on('touchmove', onDragMove);
 
         sticker.x = positionX;
         sticker.y = positionY;
 
-        return sticker;
+        stage.addChild(sticker);
     };
+
+    function onDragStart(event)
+    {
+        // store a reference to the data
+        // the reason for this is because of multitouch
+        // we want to track the movement of this particular touch
+        this.data = event.data;
+        this.alpha = 0.5;
+        this.dragging = true;
+    }
+
+    function onDragEnd()
+    {
+        this.alpha = 1;
+
+        this.dragging = false;
+
+        // set the interaction data to null
+        this.data = null;
+    }
+
+    function onDragMove()
+    {
+        if (this.dragging)
+        {
+            const newPosition = this.data.getLocalPosition(this.parent);
+            this.position.x = newPosition.x;
+            this.position.y = newPosition.y;
+        }
+    }
 
     useEffect(() => {
         const canvas = document.getElementById('canvas');
@@ -39,13 +86,11 @@ function App() {
             height: 600,
             backgroundColor: 0x1099bb
         });
+        stage = app.stage;
 
         loader.onComplete.add(() => {
-            const sticker = createSticker(100, 100);
-            app.stage.addChild(sticker);
-
-            const sticker2 = createSticker(200, 400);
-            app.stage.addChild(sticker2);
+            createSticker(100, 100);
+            createSticker(200, 400);
         });
 
     }, [canvas]);
