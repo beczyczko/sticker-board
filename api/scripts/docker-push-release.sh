0fa2a2ae5ced367aa15a9cfbca71f491284b6123
Dominik <d.beczyczko@gmail.com>
2020-05-19 20:27:21 +0200

    cleanup azure-pipelines.yml file

--- api/scripts/docker-push-release.sh
+++ api/scripts/docker-push-release.sh
@@ -1,2 +1,5 @@
 docker tag sticker-board/api:prod stickerboard.azurecr.io/sb.api:prod
 docker push stickerboard.azurecr.io/sb.api:prod
+
+
+docker tag stickerboard.azurecr.io/sb.api:<branch_name> stickerboard.azurecr.io/sb.api:prod
