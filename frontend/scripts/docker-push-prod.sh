a5c9a894b7f8edac999ff7e3d5db38f66aa4588d
Dominik B <dbeczyczko@gmail.com>
2020-07-21 23:25:37 +0200

    update docker-push scripts

--- frontend/scripts/docker-push-prod.sh
+++ frontend/scripts/docker-push-prod.sh
@@ -1,2 +1,2 @@
-docker tag sticker-board/frontend:prod stickerboard.azurecr.io/sb.frontend:prod
-docker push stickerboard.azurecr.io/sb.frontend:prod
+docker tag sticker-board/frontend:prod beczyczko/sb.frontend:prod
+docker push beczyczko/sb.frontend:prod
